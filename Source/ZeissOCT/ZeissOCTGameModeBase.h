a0a8c6a982fa34075c7e5b2572817942c1a95ac8
Aleksandra Dokic <ga87dov@mytum.de>
2018-02-01 15:46:14 +0100

    Initial code commit

---
+++ Source/ZeissOCT/ZeissOCTGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ZeissOCTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ZEISSOCT_API AZeissOCTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
